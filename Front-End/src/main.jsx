6167c6e4eeaf1387cc846e7aeab1616f613571ca
Preet Brahmbhatt <preet.brahmbhatt.code@gmail.com>
2025-05-07 15:41:24 +0530

    Fix

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -6,9 +6,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId =
-  "249431108114-gbv8cniho9utvspi8nhqdpc3f3j51d7g.apps.googleusercontent.com";
-// const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
